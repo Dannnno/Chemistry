32b345de0535a25fa5acfda3705fc041564fb8f6
Dan Obermiller <dannnno16@gmail.com>
2014-08-10 22:25:42 -0500

    Continuing work with CML
    Expanded the CMLParser class and began working on the CMLBuilder class.
    Added some more informal testing, once the CMLParser/Builder are in an
    alpha stage will start working on unit testing

---
+++ CML work/cml.py
@@ -0,0 +1,195 @@
+"""This module is used to parse CML (Chemical Markup Language) an
+extension of XML.  The XML library is extended to work with the
+tags and structure of the CML language.  
+"""
+# Needs work improving run time for larger molecules
+# Need to consider the iterparse option
+# Consider other support
+import os
+
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+    
+
+class CMLException(Exception):
+    """The base CML Exception class"""
+    
+    def __init__(self, err_message="Problem parsing CML file"):
+        """The exception should generally be passed a more specific message"""
+        self.message = err_message
+        
+    def __repr__(self):
+        return self.message
+        
+    def __str__(self):
+        return self.message
+
+
+class CMLParser(object):
+    """The base CMLParser class"""
+    
+    def __init__(self, CML): 
+        """CML should be an (open) file object or a string filepath"""
+        
+        self.molecule = {}
+        self.atoms = {}
+        self.bonds = {}
+        self.CML = CML
+        self.molecule["atoms"] = self.atoms
+        self.molecule["bonds"] = self.bonds
+        
+        try:
+            if isinstance(self.CML, file): 
+                if self.CML.closed:
+                    raise CMLException("CML file has been closed")
+        
+            elif isinstance(CML, basestring):
+                self.CML = open(self.CML, "r")
+                
+            else:
+                raise CMLException("Not a valid CML file")
+        
+        except CMLException as e:
+            print e
+            return
+            
+        else:
+            self.tree = ET.parse(self.CML)
+            try:
+                elements = self.tree.findall("atomArray")
+                if elements == []: raise CMLException("No atomArray in the molecule")
+                for element in elements:
+                    sub_elements = element.findall("atom")
+                    if sub_elements == []: raise CMLException("No atoms in the atomArray")
+                    for sub_element in sub_elements:
+                        subs = sub_element.findall("string")
+                        if subs == []: raise CMLException("Atom has no information")                
+                        for sub in subs:
+                            self.atoms[sub_element.attrib['id']] = sub.text
+            
+                # print "Atoms in molecule ", self.atoms
+                
+                elements = self.tree.findall("bondArray")
+                if elements == []: raise CMLException("No bondArray in the molecule")
+                for element in elements:
+                    sub_elements = element.findall("bond")
+                    if sub_elements == []: raise CMLException("No bonds in the bondArray")                            
+                    for sub_element in sub_elements:
+                        self.bonds[sub_element.attrib['id']] = ()
+                        subs = sub_element.findall("string")
+                        if subs == []: raise CMLException("Bond has no information")
+                        for sub in subs:
+                            self.bonds[sub_element.attrib['id']] += (sub.text,)
+                
+                # print "Bonds in molecule ", self.bonds       
+  
+            except CMLException as e:
+                print "Aborting:", e
+            
+            # print "Molecule ", self.molecules    
+
+        finally:
+            self.CML.close()
+            
+tags = ["atomArray", 
+        "molecule", 
+        "atom", 
+        "bond", 
+        "bondArray", 
+        "string"]
+        
+attributes = ["builtin=", 
+              "convention=", 
+              "title=", 
+              "id=", 
+              "type="]
+
+builtin = ["atomRef", 
+           "order", 
+           "elementType"]        
+
+
+def insert_attributes(tag, attributes, values): 
+    """Takes a desired tag, attribute to add and the value of that attribute.
+    Returns a tuple of form ("<tag attribute=value ...>", r"<\tag>").  
+    Attribute and value are both lists (of equal length) that will be
+    placed in the provided order.
+    """
+    
+    try:
+        if len(attributes) != len(values):
+            print ("The number of attributes %d does not equal the number of values %d"
+                    % len(attributes), len(values))
+            raise CMLException("Attributes and Values do not match")
+            
+        while tag not in tags:
+            tag_in = False
+            print ("Tag %s is not in the list of expected tags " % tag, tags)
+            print "Would you like to use a different tag?"
+            while True:
+                yes = raw_input("Please indicate yes/no ")
+                if yes == "yes":
+                    print "Okay, lets pick a different tag"
+                    tag = raw_input("Please enter the tag you would like to use: ")
+                    break
+                else:
+                    print "Okay, we'll use that tag"
+                    tag_in = True
+                    break
+                    
+        end_tag = "".join(["</", tag, ">"])
+        start_tag = ''.join(["<", tag])
+        for attribute, value in zip(attributes, values):
+            if "=" not in attribute:
+                attribute += "="
+            while attribute == "builtin=" and value not in builtin:
+                print "Value %s is not in the expected values for builtin: " % value, builtin
+                go = raw_input("Would you like to use that value anyway (yes/no)? ")
+                if go == yes:
+                    break
+                else:
+                    value = raw_input("Please enter the value you would like to use instead")
+            
+            start_tag = ''.join([start_tag, " %s\"%s\"" % (attribute, value)])
+            if attribute != attributes[-1]:
+                "".join([start_tag, " "])
+        start_tag += ">"
+        return start_tag, end_tag
+        
+    except CMLException as e:
+        print e
+        return
+        
+        
+def insert_text(): pass
+       
+a = insert_attributes("molecule", 
+                         ["convention", "title", "type", "id"],
+                         ["some", "Water", "", "m1"])
+   
+with open("C:/Users/Dan/Desktop/CML_3.cml", "w") as f:
+    f.write(a[0])
+    f.write(a[1])
+                                                               
+class CMLBuilder(object):  
+    """A CML constructor object"""
+    
+    def __init__(self, molecule, filename):
+        """Takes a molecule dictionary (same format as would be produced by
+        cml.CMLParser().molecule and a filename to write the molecule to
+        """        
+        
+        self.molecule = molecule
+        self.filename = filename
+        
+        if os.path.exists(filename):
+            print "Path %s exists - continue?" % filename
+            while True:
+                go = raw_input("Please enter one of the following: Yes, yes, Y or y if you would like to continue")
+                if go in ["Yes", "yes", "Y", "y"]: break
+                else: return
+        
+        with open(filename, "w") as f: 
+            base = self.molecule.keys()[0]            
---
+++ CML work/cml_tester.py
@@ -0,0 +1,53 @@
+"""This program is intended to test the various aspects of my parser
+
+"""
+
+from cml import CMLParser as CMLP, CMLException
+import os
+import cml
+print cml
+print type(cml)
+#a = cml.insert_attributes
+
+def clean_print_molecule(adict):
+    
+    if not isinstance(adict, dict):
+        print "Not a dictionary"
+        return
+        
+    for key in sorted(adict.keys()):
+        print key
+        if isinstance(adict[key], dict):
+            # print("\t", end="") - only for python 3
+            print " "
+            clean_print_molecule(adict[key])
+        else:
+            print "", adict[key]
+        if key == sorted(adict.keys())[-1]:
+            print "\n"
+    
+                            
+filepaths = []
+directory = "C:/Users/Dan/Desktop/Programming/1 - GitHub/Chemistry/CML work/"
+filepaths.append(''.join([directory, "CML_1.cml"]))
+filepaths.append(''.join([directory, "CML_2.cml"]))
+
+molecules = {}
+try:
+    broken_files = []
+    for i, afile in enumerate(filepaths):
+        if os.path.exists(afile):
+            a_molecule = CMLP(afile)
+            molecules[''.join(["m", str(i+1)])] = a_molecule.molecule
+        else:
+            broken_files.append(afile)
+    if broken_files:
+        raise CMLException("Broken File(s) at:\n " + "\n ".join(broken_files))
+
+except CMLException as e:
+    print e
+    
+print "\nMolecules parsed:\n"
+clean_print_molecule(molecules)
+for mol_id in molecules:
+    print mol_id, molecules[mol_id]
