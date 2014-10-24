c071f64fb188e2e33e6b30937391d143779d9e55
Dannnno <dannnno16@gmail.com>
2014-10-23 23:32:26 -0700

    Continuation of the graphing approach
    Moved this into a new branch so things are clearer

--- compound_graphs.py
+++ compound_graphs.py
@@ -1,8 +1,111 @@
+"""
+Copyright (c) 2014 Dan Obermiller
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+You should have received a copy of the MIT License along with this program.
+If not, see <http://opensource.org/licenses/MIT>
+"""
+
+from collections import OrderedDict
 from functools import partial
+import csv
 import json
+import os
+    
+    
+## My global functions
         
+def convert_type(cell, typ):
+    """Credit to SO user Marius for (most of) this function
+    http://stackoverflow.com/a/25498445/3076272
+
+    Takes a string and a function that the string should be represented as,
+    if possible.  If the function can't be applied to that string then it returns
+    "No_Data"
+    """
+
+    try:
+        if typ is str_to_list:
+            return typ(cell, mapped=int)
+        return typ(cell)
+    except (TypeError, ValueError):
+        return "No_Data" # The nomiker I have chosen for missing data
+        
+def str_to_list(a_stringy_list, mapped=None):
+    """Takes a string that looks like a list and makes it a list
+    Usage:
+        >>> str_to_list("[1, 2, 3]")
+        ["1", "2", "3"]
+    If mapped is provided a function it will map that function to the list
+    """
+    the_list = a_stringy_list[1:-1].split(",")
+    try:
+        # Fun fact, mapping 'None' to a list just returns the list
+        return map(mapped, the_list)
+    except ValueError: #Exception:
+        ## print "Function %s couldn't be mapped to list " % str(mapped), the_list
+        return the_list
+        
+def read_periodic_table(filename):
+    """Reads a csv file that represents all elements and pertinant data regarding
+    them and then returns them as an OrderedDict
+    """
+
+    per_table = OrderedDict()
+    
+    with open(filename, "r") as f:
+        my_reader = csv.reader(f)
+        my_reader.next() # skips the header
+        for i in range(118):
+            tl = my_reader.next()
+            col_types = [int, str, str, int, float, float, float,
+                         float, float, float, float, str_to_list]
+            new_row = tuple(convert_type(cell, typ)
+                            for cell, typ in zip(tl, col_types))
+            per_table[tl[1]] = new_row
+    
+    return per_table
+    
+# Populates my periodic table with all of the element data
+try:
+    # For some reason it needs the rest of the path when I run this, but it 
+    # doesn't when I run the unit tests...
+    periodic_table = read_periodic_table(os.getcwd()+"/desktop/programming/github/chemistry/element_list.csv") 
+except IOError:
+    periodic_table = read_periodic_table(os.getcwd()+"/element_list.csv") 
 
 class Compound(object): 
+    
+    @classmethod
+    def from_CML(cls, CML_file):
+        """An alternate constructor if I'd like to load this directly from a 
+        CML file
+        """
+        raise NotImplementedError
+        
+    @classmethod
+    def json_serialize(cls, obj):
+        try:
+            return obj.__dict__
+        except AttributeError:
+            return map(repr, obj)
 
     def __init__(self, atoms, bonds):
         self.atoms = {key:Element(value) for key, value in atoms.iteritems()}
@@ -16,31 +119,37 @@ class Compound(object):
                                       sort_keys=True, 
                                       indent=4),
                               [self.atoms, self.bonds]))
-                              
-    @classmethod
-    def json_serialize(cls, obj):
-        try:
-            return obj.__dict__
-        except AttributeError:
-            return map(repr, obj)
 
 
 class Element(object): 
 
     def __init__(self, symbol='C'):
-        self.symbol = symbol
         self.bonds = set()
         self.bonded_to = set()
+
+        self.symbol = symbol
+        self.number = periodic_table[symbol][0]
+        self.name = periodic_table[symbol][2]
+        self.group = periodic_table[symbol][3] 
+        self.weight = periodic_table[symbol][4]
+        self.density = periodic_table[symbol][5]
+        self.mp = periodic_table[symbol][6]
+        self.bp = periodic_table[symbol][7]
+        self.eneg = periodic_table[symbol][9]
+        self.radius = periodic_table[symbol][10]
+        self.oxid = periodic_table[symbol][11]
+        
         
     def create_bond(self, a_bond, an_element):
         self.bonds.add(a_bond)
         self.bonded_to.add(an_element)
         
-    def break_bond(self, a_bond):
+    def break_bond(self, a_bond, flag=False):
         other = a_bond.get_other(self)
         self.bonds.discard(a_bond)
         self.bonded_to.discard(other)
-        other.break_bond(a_bond)
+        if not flag:
+            other.break_bond(a_bond, True)
         
     def __str__(self):
         return self.symbol
@@ -52,6 +161,13 @@ class Element(object):
 class Bond(object): 
 
     def __init__(self, element1, element2, order=1, chirality=None):
+        if order not in xrange(1, 4):
+            raise ValueError(
+                "The order of a bond must be [1,3], not {}".format(order))
+        if chirality not in [None, 'R', 'S', 'E', 'Z']:
+            raise ValueError(''.join(["A bond must have no ",
+                                        "(None) chirality or R/S/E/Z ",
+                                        "chirality, not {}"]).format(chirality))
         self.order = order
         self.chirality = chirality
         self.bond = set([element1, element2])
@@ -76,7 +192,6 @@ class Bond(object):
                                                   self.second, 
                                                   self.order, 
                                                   self.chirality)
-                
-a = Compound({'a1':'O', 'a2':'H', 'a3':'H'},
-             {'b1':('a1', 'a2', 1), 'b2':('a1', 'a3', 1)})                
-print a
+     
+if __name__ == '__main__':           
+    pass
---
+++ test_compound_graphs.py
@@ -0,0 +1,141 @@
+try:
+    import cStringIO as IO
+except ImportError:
+    import StringIO as IO
+finally:
+    from collections import OrderedDict
+    import compound_graphs as cg
+    import contextlib
+    import os
+    import sys
+    import unittest
+
+@contextlib.contextmanager
+def capture():
+    oldout, olderr = sys.stdout, sys.stderr
+    
+    try:
+        out=[IO.StringIO(), IO.StringIO()]
+        sys.stdout, sys.stderr = out
+        yield out
+        
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+
+class test_global_functions(unittest.TestCase): 
+
+    def test_convert_type(self):
+        self.assertEqual(cg.convert_type('1', int), 1)
+        self.assertEqual(cg.convert_type('1.0', float), 1.0)
+        self.assertEqual(cg.convert_type('Hello', str), 'Hello')
+        self.assertEqual(cg.convert_type('[1,2,3]', cg.str_to_list), [1, 2, 3])
+        
+    def test_str_to_list(self):
+        self.assertEqual(cg.str_to_list('[1,2,3]'), ['1', '2', '3'])
+        self.assertEqual(cg.str_to_list('[1,2,3]', mapped=int), [1, 2, 3])
+
+    def test_read_periodic_table(self):
+        col_types = [int, str, str, int, float, float, float,
+                     float, float, float, float, cg.str_to_list]
+        line = ["6","C","Carbon","14","12.011","2.267","3800",
+                "4300","0.709","2.55","67","[1,2,3,4,-4,-3,-2,-1]"]
+        self.assertIs(os.path.exists(os.getcwd() + "/element_list.csv"), True)
+        self.assertIs(isinstance(cg.read_periodic_table("element_list.csv"), 
+                                 OrderedDict), True)
+        self.assertEqual(cg.read_periodic_table("element_list.csv")["C"],
+                         tuple((cg.convert_type(cell, typ)
+                                for cell, typ in zip(line, col_types))))
+
+class test_compound(unittest.TestCase): 
+    
+    def test_from_CML(self):
+        self.assertRaises(NotImplementedError,
+                          cg.Compound.from_CML,
+                          "any_file_name.cml")
+                          
+    def test_json_serializer(self):
+        self.assertEqual(cg.Compound.json_serialize(cg.Element()),
+                         {'oxid': [1, 2, 3, 4, -4, -3, -2, -1], 
+                          'group': 14, 'name': 'Carbon', 'weight': 12.011, 
+                          'bonds': set([]), 'symbol': 'C', 'density': 2.267,
+                          'number': 6, 'eneg': 2.55, 'bp': 4300.0, 
+                          'bonded_to': set([]), 'radius': 67.0, 'mp': 3800.0})
+        a = cg.Element()
+        b = cg.Element()
+        ab = cg.Bond(a, b)                          
+        self.assertEqual(cg.Compound.json_serialize(ab),
+                         {'second': b,
+                          'first': a,
+                          'chirality': None, 'order': 1, 
+                          'bond': set([a,b])})
+        self.assertEqual(cg.Compound.json_serialize(set([1, 2, 3])),
+                         ['1', '2', '3'])
+
+
+class test_element(unittest.TestCase): 
+    
+    def test_create_bond(self): 
+        """This doesn't actually get tested very well because Bond.__init__()
+        automatically adds the bond and element to a's sets.
+        """
+        a = cg.Element()
+        b = cg.Element()
+        ab = cg.Bond(a, b)
+        a.create_bond(ab, b)
+        self.assertIn(ab, a.bonds)
+        self.assertIn(a, b.bonded_to)
+    
+    def test_break_bond(self): 
+        a = cg.Element()
+        b = cg.Element()
+        ab = cg.Bond(a, b)
+        a.break_bond(ab)
+        self.assertNotIn(ab, a.bonds)
+        self.assertNotIn(ab, b.bonds)
+        self.assertNotIn(a, b.bonded_to)
+        self.assertNotIn(b, a.bonded_to)
+
+
+class test_bond(unittest.TestCase): 
+
+    def setUp(self): 
+        self.elements = [cg.Element(), cg.Element(), cg.Element(),
+                         cg.Element(), cg.Element(), cg.Element()]
+        self.b1 = cg.Bond(self.elements[0], self.elements[1])
+        self.b2 = (self.elements[2], self.elements[3], 4)
+        self.b3 = (self.elements[4], self.elements[5], 1, 'W')
+    
+    def test_constructor(self):
+        ## Testing that it throws a ValueError when it should
+        self.assertRaises(ValueError, cg.Bond, *self.b2)
+        self.assertRaises(ValueError, cg.Bond, *self.b3)
+        
+        ## Testing that its updating the Elements' sets
+        self.assertIn(self.b1, self.elements[0].bonds)
+        self.assertIn(self.elements[1], self.elements[0].bonded_to)
+        
+    def test_get_other(self):
+        self.assertEqual(self.b1.first, self.b1.get_other(self.b1.second))
+        self.assertRaises(KeyError, self.b1.get_other, self.elements[3])
+
+
+if __name__ == '__main__':
+    test_classes_to_run = [test_global_functions, 
+                           test_compound, 
+                           test_element, 
+                           test_bond]
+
+    loader = unittest.TestLoader()
+
+    suites_list = []
+    for test_class in test_classes_to_run:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites_list.append(suite)
+
+    big_suite = unittest.TestSuite(suites_list)
+
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=1)
+    runner.run(big_suite)
